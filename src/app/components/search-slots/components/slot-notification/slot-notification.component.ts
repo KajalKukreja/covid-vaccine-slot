bae7c04feee68c20866e6888a1f3a5239bd158f3
Kajal Kukreja <kajalkukreja694@gmail.com>
2021-05-16 19:09:29 +0530

    Some formatting changes

--- src/app/components/search-slots/components/slot-notification/slot-notification.component.ts
+++ src/app/components/search-slots/components/slot-notification/slot-notification.component.ts
@@ -13,7 +13,7 @@ import { StatesService } from 'src/app/services/states.service';
 	providers: [StatesService, DistrictsService, MemberService]
 })
 export class SlotNotification implements OnInit {
-	
+
 	pincode: number;
 
 	states: State[];
@@ -23,17 +23,17 @@ export class SlotNotification implements OnInit {
 	districts: District[];
 
 	selectedDistrict: District;
-	
+
 	displayEmail: boolean = false;
 
 	displayWhatsApp: boolean = false;
-  
+
 	email: string;
-  
+
 	mobileNo: number;
-  
+
 	emailAdded: string;
-  
+
 	mobileNoAdded: string;
 
 	constructor(private router: Router, private route: ActivatedRoute, private statesService: StatesService, private districtsService: DistrictsService, private memberService: MemberService) {
@@ -47,9 +47,9 @@ export class SlotNotification implements OnInit {
 
 	convertInput() {
 		if (this.pincode != null && this.pincode != undefined) {
-		  this.pincode = Math.abs(this.pincode);
+			this.pincode = Math.abs(this.pincode);
 		}
-	  }
+	}
 
 	showStates() {
 		this.statesService.getStates().subscribe((rowData: any) => {
@@ -79,7 +79,7 @@ export class SlotNotification implements OnInit {
 			});
 		}
 	}
-	
+
 	showEmailDialog() {
 		this.pincode = null;
 		this.selectedState = null;
@@ -89,9 +89,9 @@ export class SlotNotification implements OnInit {
 		this.emailAdded = null;
 		this.mobileNoAdded = null;
 		this.displayEmail = true;
-	  }
-	
-	  showWhatsAppDialog() {
+	}
+
+	showWhatsAppDialog() {
 		this.pincode = null;
 		this.selectedState = null;
 		this.selectedDistrict = null;
@@ -100,29 +100,29 @@ export class SlotNotification implements OnInit {
 		this.emailAdded = null;
 		this.mobileNoAdded = null;
 		this.displayWhatsApp = true;
-	  }
-	
-	  notifyMe() {
+	}
+
+	notifyMe() {
 		if (this.displayEmail && this.validEmail()) {
-		  this.memberService.addMember(this.email, null, this.pincode, 
-			this.selectedDistrict != null ? this.selectedDistrict.id : null).subscribe((result: any) => {
-			result ? this.emailAdded = 'true': this.emailAdded = 'false';
-		  });
+			this.memberService.addMember(this.email, null, this.pincode,
+				this.selectedDistrict != null ? this.selectedDistrict.id : null).subscribe((result: any) => {
+					result ? this.emailAdded = 'true' : this.emailAdded = 'false';
+				});
 		}
 		else if (this.displayWhatsApp && this.validMobileNo()) {
-			this.memberService.addMember(null, this.mobileNo, this.pincode, 
+			this.memberService.addMember(null, this.mobileNo, this.pincode,
 				this.selectedDistrict != null ? this.selectedDistrict.id : null).subscribe((result: any) => {
-				result ? this.mobileNoAdded = 'true': this.mobileNoAdded = 'false';
-			  });
+					result ? this.mobileNoAdded = 'true' : this.mobileNoAdded = 'false';
+				});
 		}
-	  }
-	
-	  validEmail(): boolean {
+	}
+
+	validEmail(): boolean {
 		return (this.email != null && this.email != undefined && this.email.length > 0);
-	  }
-	
-	  validMobileNo(): boolean {
+	}
+
+	validMobileNo(): boolean {
 		return (this.mobileNo != null && this.mobileNo != undefined && this.mobileNo != 0);
-	  }
-	
+	}
+
 }
